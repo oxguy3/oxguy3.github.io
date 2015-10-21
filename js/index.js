4bba757d15671bf05c60bd0c55f3f1ccc616231f
Hayden Schiff <oxguy3@gmail.com>
2015-10-21 12:10:08 -0400

    cleaned up JS, fixed tab scrolling bug

--- js/index.js
+++ js/index.js
@@ -6,47 +6,25 @@ $('#navbarTablist a').click(function (e) {
   
   // update the url hash
   location.hash = e.target.hash;
-  //$(window).scrollTop();
-  window.scrollTo(0, 0);         // execute it straight away
-  setTimeout(function() {
-    window.scrollTo(0, 0);     // run it a bit later also for browser compatibility
-  }, 1);
-
-  // // hide the stuff we're gonna fade in
-  // var tabDiv = $(location.hash);
-  // tabDiv.finish("fadeboxes");
-  // var fadeboxes = tabDiv.find(".fadebox");
-  // fadeboxes.css("display", "none");
-  // $(".fadebox-post").css("display", "none");
   
   // switch tabs
   $(this).tab('show');
   
   // update the page title
-  document.title = PAGE_TITLE_PREFIX + $(this).html();
+  document.title = PAGE_TITLE_PREFIX + $(this).text();
   
   // hide the navbar dropdown on XS screens
   $("#navbarCollapse").removeClass("in");
 
-
-  // fadeboxes.each(function() {
-  //   addToFadeboxesQueue($(this), tabDiv);
-  // });
-  // addToFadeboxesQueue($(".fadebox-post"), tabDiv);
-
-  // tabDiv.dequeue("fadeboxes");
-
 });
 
-// function addToFadeboxesQueue(box, tabDiv) {
-//   tabDiv.queue("fadeboxes", function() {
-//     box.fadeIn({
-//       slideDown: 1500
-//     });
-//     tabDiv.dequeue("fadeboxes");
-//   })
-//   tabDiv.delay(200, "fadeboxes");
-// }
+$('#navbarTablist a[href^=#]').on('shown.bs.tab', function (e) {
+  // scroll to top of tab
+  window.scrollTo(0, 0); // execute it straight away
+  setTimeout(function() {
+    window.scrollTo(0, 0); // run it a bit later also for browser compatibility
+  }, 1);
+})
 
 $('a.homebox-link[href^=#]').click(function (e) {
   var href = $(this).attr("href");
@@ -70,11 +48,6 @@ $(document).ready(function() {
   
   $(".tab-pane").addClass("fade");
   $("#"+hash).addClass("in");
-
-
-
-  // apply pretty colors!
-  //prettifyHomePage();
   
 });
 
@@ -84,9 +57,6 @@ $('#navbarTablistHome').click(function() {
 
 function prettifyHomePage() {
   // colors taken from http://flatuicolors.com/
-  /*var prettyColors = ["#1ABC9C", "#16A085", "#2ECC71", "#27AE60", "#3498DB", 
-    "#2980B9", "#9B59B6", "#8E44AD", "#34495E", "#2C3E50", "#F1C40F", 
-    "#F39C12", "#E67E22", "#D35400", "#E74C3C", "#C0392B"];*/
   var prettyColors = [
     "#c0392b", // resume
     "#16a085", // projects
@@ -100,7 +70,7 @@ function prettifyHomePage() {
 
     $(this).css("color", "transparent");
 
-    var index = 0; //Math.floor(Math.random()*prettyColors.length);
+    var index = 0;
     var currColor = (prettyColors.length > 0) ? prettyColors[index] : "#000000";
 
     $(this).animate({
@@ -108,7 +78,6 @@ function prettifyHomePage() {
     }, 1500);
     prettyColors.splice(index, 1);
   });
-  //setTimeout(prettifyHomePage, 5000);
 }
 
 // tooltips for skills
